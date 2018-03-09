e0c137415880ad656333c3af713ff1234e847614
PeterAdamson <adamsonpeterj@gmail.com>
2018-03-09 18:52:48 -0400

    Update PolicyCounter.py
    Script rework to reflect new logic

--- PolicyCounter.py
+++ PolicyCounter.py
@@ -9,6 +9,8 @@ import boto3
 import botocore
 import json
 
+seen = []
+
 BUCKET_NAME = 'user.policies'
 KEY = 'user_policies.json'
 
@@ -16,30 +18,31 @@ s3 = boto3.resource('s3')
 count = 0
 
 try:
-    s3.Bucket(BUCKET_NAME).download_file(KEY, 'user_policies.json')
+	s3.Bucket(BUCKET_NAME).download_file(KEY, 'user_policies.json')
 except botocore.exceptions.ClientError as e:
-    if e.response['Error']['Code'] == '404':
-        print('The object does not exist.')
-    else:
-        raise Exception('An error occurred.')
+	if e.response['Error']['Code'] == '404':
+		print('The object does not exist.')
+	else:
+		raise Exception('An error occurred.')
 
 with open('user_policies.json') as json_data:
-    jdata = json.load(json_data)
-    for data in jdata['data']['users']['items']:
-        for further in data['policies']['items']:
-            if 'FullAccess' in list(further.values())[0]:
-                count += 1
+	jdata = json.load(json_data)
+	for data in jdata['data']['users']['items']:
+		for further in data['policies']['items']:
+			if 'FullAccess' in list(further.values())[0] and list(further.values())[0] not in seen:
+				count += 1
+				seen.append(list(further.values())[0])
 
 store = {'count': count}
 
 try:
-    with open('count.json', 'w') as outfile:
-        json.dump(store, outfile)
+	with open('count.json', 'w') as outfile:
+		json.dump(store, outfile)
 except IOError:
-    print('Unable to create file')
+	print('Unable to create file')
 
 try:
-    with open('count.json', 'rb') as upload:
-        s3.Bucket(BUCKET_NAME).put_object(Key='count.json', Body=upload)
+	with open('count.json', 'rb') as upload:
+	s3.Bucket(BUCKET_NAME).put_object(Key='count.json', Body=upload)
 except FileNotFoundError:
-    print('Wrong file or file path')
+	print('Wrong file or file path')
